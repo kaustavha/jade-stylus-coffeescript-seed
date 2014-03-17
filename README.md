ad28f5f28416c2713dd6ade215a139f653133a2f
kaustavha <kaustav_2007@hotmail.com>
2014-03-16 21:56:28 -0400

    initial commit -- working seed

---
+++ README.md
@@ -0,0 +1,12 @@
+#jade-stylus-coffeescript-seed
+
+This is an ultra minimal seed for building static websites with JADE, stylus and coffeescript. GulpJS is used to build. 
+The source files are in the src directory.
+The gulpfile only compiles the three src files with their current names, no directory walker or options for multiple sources is included currently. 
+
+## Steps:
+From the command line, navigate to the main directory and run
+'''npm install'''
+'''gulp'''
+
+Gulp returns an index.html in the main directory, and scripts.js and styles.css in the assets directory. It will also watch  files in the src directory for changes and recompile. 
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    log = require('gulp-util').log,
+    rename = require('gulp-rename'),
+    jade = require('gulp-jade'),
+    stylus = require('gulp-stylus'),
+    coffee = require('gulp-coffee');
+
+gulp.task('templates', function() {
+	var locs = {};	 
+	gulp.src('./src/jade.jade')
+	    .pipe(jade({locals: locs}))
+	    .pipe(rename('index.html'))
+	    .pipe(gulp.dest('./'))
+});
+gulp.task('scripts', function() {
+	gulp.src('./src/coffee.cs')
+	    .pipe(coffee({bare: true}).on('error', log))
+	    .pipe(rename('scripts.js'))
+	    .pipe(gulp.dest('./assets'))
+});
+gulp.task('styles', function() {
+           gulp.src('./src/stylus.styl')
+	           .pipe(stylus())
+	           .pipe(rename('styles.css'))
+	           .pipe(gulp.dest('./assets'))
+});
+gulp.task('watch', function() {
+	log('Watching files');
+	gulp.watch('./src/*', ['build']);
+});
+
+//define cmd line default task
+gulp.task('build', ['templates', 'styles', 'scripts']);
+gulp.task('default', ['build', 'watch']);
