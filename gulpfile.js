6d9fba8b80ed9ae7236cf9d5e1659f0f3f8fb3ed
kaustavha <hi@kaustav.me>
2015-04-05 06:21:26 -0400

    added browsersync, changed path to public

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,41 @@ var gulp = require('gulp'),
     log = require('gulp-util').log,
     jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    browserSync = require('browser-sync');
 
 gulp.task('templates', function() {
   var locs = {};
-  gulp.src('./src/index.jade')
+  gulp.src('./src/**/*.jade')
     .pipe(jade({locals: locs}))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./public'))
 });
 
 gulp.task('scripts', function() {
-  gulp.src('./src/scripts.coffee')
+  gulp.src('./src/**/*.coffee')
     .pipe(coffee({bare: true}).on('error', log))
-    .pipe(gulp.dest('./assets'))
+    .pipe(gulp.dest('./public/assets'))
 });
 
 gulp.task('styles', function() {
-  gulp.src('./src/styles.styl')
+  gulp.src('./src/**/*.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('./assets'))
+    .pipe(gulp.dest('./public/assets'))
 });
 
 gulp.task('watch', function() {
 	log('Watching files');
-	gulp.watch('./src/*', ['build']);
+	gulp.watch('./src/**/*', ['build']);
+});
+
+gulp.task('browserSync', ['build'], function() {
+  browserSync({
+    server: {
+      baseDir: './public'
+    }
+  });
 });
 
 //define cmd line default task
 gulp.task('build', ['templates', 'styles', 'scripts']);
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'watch', 'browserSync']);
